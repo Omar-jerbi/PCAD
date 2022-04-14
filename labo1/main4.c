d49f30b94edf011d0a51c37421430a9a8eb5e3a7
Omar-jerbi <67864737+Omar-jerbi@users.noreply.github.com>
2022-04-14 15:53:51 +0200

    Update main4.c

--- labo1/main4.c
+++ labo1/main4.c
@@ -5,16 +5,15 @@
 #include <sys/time.h>
 #include <string.h>
 
+
+
 pthread_barrier_t barrierAB;
 pthread_barrier_t barrierCR;
 
-// const int NTh = 2;
-// const int NBlocks = 2; 
 
-
-const int M = 8;//IMPORTANT: M deve essere multiplo di NBlocks!
-const int N = 8;
-const int P = 4;
+const int M = 80;
+const int N = 80;
+const int P = 40;
 
 
 float** A; //M*N //8*8
@@ -83,7 +82,7 @@ pthread_barrier_wait(&barrierCR);
 
 }
 
-///////////////////////////////MAIN///////////////////////////////////////
+/////////////////////////////////////////////////////////////////MAIN/////////////////////////////////////////////////////////
 int main(int argc, char** argv){
     //parte init matrici
 /********************************************************/
@@ -138,138 +137,135 @@ int main(int argc, char** argv){
     
 
 
-if(argc != 4){
-    printf("INPUT= PROGRAMMA NUMERO_THREAD(0 se sequenziale) NUMERO_BLOCCHI_A NUMERO_BLOCCHI_C\n");
-    return EXIT_FAILURE;
-}
-
-int nThrds = atoi(argv[1]);
-int nBlcksA = atoi(argv[2]);
-int nBlcksC = atoi(argv[3]);
+    if(argc != 4){
+        printf("INPUT=prog NUMERO_BLOCCHI_A NUMERO_BLOCCHI_C <<'M'/'S' per Multithreaded/Sequenziale>>\n");
+        return EXIT_FAILURE;
+    }
 
 
-
-if(M % nBlcksA != 0){
-    printf("numero blocchi di A deve essere 1, 2 ,4 o 8 !\n");
-    return(EXIT_FAILURE);
-}
-
-if(P % nBlcksC != 0){
-    printf("numero blocchi di C deve essere 1, 2 o 4 !\n");
-    return(EXIT_FAILURE);
-}
+    if(argv[3][0] == 'S'){ //SINGLE THREAD!!!!!!!
+        printf("weeeeeee\n");
+        return(EXIT_SUCCESS);
+    }
 
 
+    if(argv[3][0] != 'M'){ //MULTITHREADED:
+        printf("M per MULTITHREADED / S per SINGLE-THREADED\n");
+        return(EXIT_FAILURE);
+    }else{
+        int nBlcksA = atoi(argv[1]);
+        int nBlcksC = atoi(argv[2]);
+        int nThrds = nBlcksA >= nBlcksC ? nBlcksA : nBlcksC; //il max tra blocchi di A e blocchi di C 
 
-struct timeval stop, start;
-gettimeofday(&start, NULL); //start timer
-
-
-
-
-pthread_t workers[nThrds];
-
-pthread_barrier_init(&barrierAB, NULL, nThrds + 1);
-pthread_barrier_init(&barrierCR, NULL, nThrds + 1);
-
-
-struct I_F blocks[nBlcksA >= nBlcksC ? nBlcksA : nBlcksC];
-
-blocks[0].inizioAB = 0;
-blocks[0].fineAB = M / nBlcksA;
-blocks[0].inizioCR = 0;
-blocks[0].fineCR = P/nBlcksC;
-
-if(nBlcksA >= nBlcksC){
-    for(int i = 1; i < nBlcksA; i++){
-        blocks[i].inizioAB = blocks[i-1].fineAB;
-        blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
-        
-        if(blocks[i-1].fineCR != P && blocks[i-1].fineCR != 0){
-            blocks[i].inizioCR = blocks[i-1].fineCR;
-            blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
-        }else{
-            blocks[i].inizioCR = 0;
-            blocks[i].fineCR = 0;
+        if(M % nBlcksA != 0){
+            printf("numero blocchi di A deve essere 1, 2 ,4 o 8 !\n");
+            return(EXIT_FAILURE);
         }
-    }
-}else{
-    for(int i = 1; i < nBlcksC; i++){
-        blocks[i].inizioCR = blocks[i-1].fineCR;
-        blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
-        
-        if(blocks[i-1].fineAB != M && blocks[i-1].fineAB != 0){
-            blocks[i].inizioAB = blocks[i-1].fineAB;
-            blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
-        }else{
-            blocks[i].inizioAB = 0;
-            blocks[i].fineAB = 0;
+
+        if(P % nBlcksC != 0){
+            printf("numero blocchi di C deve essere 1, 2 o 4 !\n");
+            return(EXIT_FAILURE);
         }
+
+
+
+        struct timeval stop, start;
+        gettimeofday(&start, NULL); //start timer
+
+
+
+
+        pthread_t workers[nThrds];
+
+        pthread_barrier_init(&barrierAB, NULL, nThrds + 1);
+        pthread_barrier_init(&barrierCR, NULL, nThrds + 1);
+
+
+        struct I_F blocks[nBlcksA >= nBlcksC ? nBlcksA : nBlcksC];
+
+        blocks[0].inizioAB = 0;
+        blocks[0].fineAB = M / nBlcksA;
+        blocks[0].inizioCR = 0;
+        blocks[0].fineCR = P/nBlcksC;
+
+        if(nBlcksA >= nBlcksC){
+            for(int i = 1; i < nBlcksA; i++){
+                blocks[i].inizioAB = blocks[i-1].fineAB;
+                blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
+                
+                if(blocks[i-1].fineCR != P && blocks[i-1].fineCR != 0){
+                    blocks[i].inizioCR = blocks[i-1].fineCR;
+                    blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
+                }else{
+                    blocks[i].inizioCR = 0;
+                    blocks[i].fineCR = 0;
+                }
+            }
+        }else{
+            for(int i = 1; i < nBlcksC; i++){
+                blocks[i].inizioCR = blocks[i-1].fineCR;
+                blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
+                
+                if(blocks[i-1].fineAB != M && blocks[i-1].fineAB != 0){
+                    blocks[i].inizioAB = blocks[i-1].fineAB;
+                    blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
+                }else{
+                    blocks[i].inizioAB = 0;
+                    blocks[i].fineAB = 0;
+                }
+            }
+        }
+
+
+        for(int i = 0; i<nThrds; i++){
+            pthread_create(&workers[i], NULL, PRODCAB, &blocks[i]);
+        }
+
+
+        pthread_barrier_wait(&barrierAB);
+        pthread_barrier_wait(&barrierCR);
+
+
+        //Log dei risultati/////////////////////////////////////////////////////
+                                printf("A*B\n");
+                                for(int i = 0; i<M; i++){
+                                    for(int j = 0; j< P; j++){
+                                        printf("%.1f ", R[i][j]);
+                                    }
+                                    printf("\n");
+                                }
+
+
+                                printf("\n");
+                                printf("\n");
+                                printf("\n");
+
+                                printf("C*(A*B)\n");
+                                for(int i = 0; i<P; i++){
+                                    for(int j = 0; j< P; j++){
+                                        printf("%.1f ", FINAL_RES[i][j]);
+                                    }
+                                    printf("\n");
+                                }
+        /////////////////////////////////////////////////////////////////////////
+
+
+
+        for(int i = 0; i< nThrds; i++){
+            pthread_join(workers[i], NULL);
+        }
+
+        pthread_barrier_destroy(&barrierAB);
+        pthread_barrier_destroy(&barrierCR);
+
+
+
+
+
+        gettimeofday(&stop, NULL);
+        printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
+
     }
-}
 
-//blocks testati OK
-
-
-
-
-
-
-
-//metti a posto questione input num thread, togli NTh
-//crea array con le struct che dai in pasto a thread, ciclo for per
-// creazione thread i con passaggio argomento i di array delle struct I_F (i valori della I_F i_f
-// cambieranno in base al num blocks assegnato da user == argv[2])
-
-
-    struct I_F i_f = {0, 4, 0, 2};
-    struct I_F i_f2 = {4, 8, 2, 4};
-
-    pthread_create(&workers[0], NULL, PRODCAB, &i_f);
-    pthread_create(&workers[1], NULL, PRODCAB, &i_f2);
-
-
-    pthread_barrier_wait(&barrierAB);
-    pthread_barrier_wait(&barrierCR);
-
-
-                        for(int i = 0; i<M; i++){
-                            for(int j = 0; j< P; j++){
-                                printf("%f ", R[i][j]);
-                            }
-                            printf("\n");
-                        }
-
-
-                        printf("\n");
-                        printf("\n");
-                        printf("\n");
-
-
-                        for(int i = 0; i<P; i++){
-                            for(int j = 0; j< P; j++){
-                                printf("%.1f ", FINAL_RES[i][j]);
-                            }
-                            printf("\n");
-                        }
-
-
-
-
-    for(int i = 0; i< 2; i++){
-        pthread_join(workers[i], NULL);
-    }
-
-    pthread_barrier_destroy(&barrierAB);
-    pthread_barrier_destroy(&barrierCR);
-
-
-
-
-
-    gettimeofday(&stop, NULL);
-    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
-
-
-
-}
+    return(EXIT_SUCCESS);
+}//fine main
