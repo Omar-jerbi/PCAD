b7b0806c12be61467720409049f3c43c7219241f
Omar-jerbi <67864737+Omar-jerbi@users.noreply.github.com>
2022-04-14 16:37:08 +0200

    Update main4.c

--- labo1/main4.c
+++ labo1/main4.c
@@ -5,6 +5,31 @@
 #include <sys/time.h>
 #include <string.h>
 
+/* RISULTATI SU ALCUNE ESECUZIONI
+    con matrici di piccole dimensioni (8 8 4):
+        SINGLE TH:
+            INPUT: 1 * S +-= 70us
+            INPUT: * 1 S +-= 70us
+            INPUT: 8 4 S +-= 90us
+        MULTI TH:
+            INPUT: 1 * M +-= 300us
+            INPUT: 8 * M +-= 600us
+            INPUT: * 4 M +-= 400us
+        
+    con matrici di grandi dimensioni (80 80 40):
+        SINGLE TH:
+            INPUT: 1 1 S +-= 15000us // 0,014s
+            INPUT: 1 40 S +-= 5000us // 0,005s
+            INPUT: 80 1 S +-= 5000us // 0,005s
+            INPUT: 8 4 S +-= 90us
+        MULTI TH:
+            INPUT: 1 * M +-= 300us
+            INPUT: 8 * M +-= 600us
+            INPUT: * 4 M +-= 400us
+*/ 
+
+
+
 
 
 pthread_barrier_t barrierAB;
@@ -14,6 +39,9 @@ pthread_barrier_t barrierCR;
 const int M = 80;
 const int N = 80;
 const int P = 40;
+// const int M = 8;
+// const int N = 8;
+// const int P = 4;
 
 
 float** A; //M*N //8*8
@@ -135,16 +163,102 @@ int main(int argc, char** argv){
     }
 /********************************************************/
     
-
-
     if(argc != 4){
         printf("INPUT=prog NUMERO_BLOCCHI_A NUMERO_BLOCCHI_C <<'M'/'S' per Multithreaded/Sequenziale>>\n");
         return EXIT_FAILURE;
     }
 
 
+    struct timeval stop, start;//timer
+    int nBlcksA = atoi(argv[1]);
+    int nBlcksC = atoi(argv[2]);
+    int nThrds = nBlcksA >= nBlcksC ? nBlcksA : nBlcksC; //il max tra blocchi di A e blocchi di C 
+    
+    if(M % nBlcksA != 0){
+        printf("numero blocchi di A deve essere 1, 2 ,4 o 8 !\n");
+        return(EXIT_FAILURE);
+    }
+    if(P % nBlcksC != 0){
+        printf("numero blocchi di C deve essere 1, 2 o 4 !\n");
+        return(EXIT_FAILURE);
+    }
+
+    struct I_F blocks[nBlcksA >= nBlcksC ? nBlcksA : nBlcksC];
+
+    blocks[0].inizioAB = 0;
+    blocks[0].fineAB = M / nBlcksA;
+    blocks[0].inizioCR = 0;
+    blocks[0].fineCR = P/nBlcksC;
+
+    if(nBlcksA >= nBlcksC){
+        for(int i = 1; i < nBlcksA; i++){
+            blocks[i].inizioAB = blocks[i-1].fineAB;
+            blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
+            
+            if(blocks[i-1].fineCR != P && blocks[i-1].fineCR != 0){
+                blocks[i].inizioCR = blocks[i-1].fineCR;
+                blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
+            }else{
+                blocks[i].inizioCR = 0;
+                blocks[i].fineCR = 0;
+            }
+        }
+    }else{
+        for(int i = 1; i < nBlcksC; i++){
+            blocks[i].inizioCR = blocks[i-1].fineCR;
+            blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
+            
+            if(blocks[i-1].fineAB != M && blocks[i-1].fineAB != 0){
+                blocks[i].inizioAB = blocks[i-1].fineAB;
+                blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
+            }else{
+                blocks[i].inizioAB = 0;
+                blocks[i].fineAB = 0;
+            }
+        }
+    }
+
+
+
+
+
     if(argv[3][0] == 'S'){ //SINGLE THREAD!!!!!!!
-        printf("weeeeeee\n");
+        gettimeofday(&start, NULL); //start timer
+        
+        pthread_barrier_init(&barrierAB, NULL, 1);
+        pthread_barrier_init(&barrierCR, NULL, 1);
+        
+        for(int i = 0; i<nThrds; i++){
+            PRODCAB(&blocks[i]);
+        }
+
+
+        //Log dei risultati/////////////////////////////////////////////////////
+                                printf("A*B\n");
+                                for(int i = 0; i<M; i++){
+                                    for(int j = 0; j< P; j++){
+                                        printf("%.1f ", R[i][j]);
+                                    }
+                                    printf("\n");
+                                }
+
+
+                                printf("\n");
+                                printf("\n");
+                                printf("\n");
+
+                                printf("C*(A*B)\n");
+                                for(int i = 0; i<P; i++){
+                                    for(int j = 0; j< P; j++){
+                                        printf("%.1f ", FINAL_RES[i][j]);
+                                    }
+                                    printf("\n");
+                                }
+        /////////////////////////////////////////////////////////////////////////
+
+
+        gettimeofday(&stop, NULL);
+        printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
         return(EXIT_SUCCESS);
     }
 
@@ -153,25 +267,7 @@ int main(int argc, char** argv){
         printf("M per MULTITHREADED / S per SINGLE-THREADED\n");
         return(EXIT_FAILURE);
     }else{
-        int nBlcksA = atoi(argv[1]);
-        int nBlcksC = atoi(argv[2]);
-        int nThrds = nBlcksA >= nBlcksC ? nBlcksA : nBlcksC; //il max tra blocchi di A e blocchi di C 
-
-        if(M % nBlcksA != 0){
-            printf("numero blocchi di A deve essere 1, 2 ,4 o 8 !\n");
-            return(EXIT_FAILURE);
-        }
-
-        if(P % nBlcksC != 0){
-            printf("numero blocchi di C deve essere 1, 2 o 4 !\n");
-            return(EXIT_FAILURE);
-        }
-
-
-
-        struct timeval stop, start;
         gettimeofday(&start, NULL); //start timer
-
 
 
 
@@ -181,40 +277,7 @@ int main(int argc, char** argv){
         pthread_barrier_init(&barrierCR, NULL, nThrds + 1);
 
 
-        struct I_F blocks[nBlcksA >= nBlcksC ? nBlcksA : nBlcksC];
 
-        blocks[0].inizioAB = 0;
-        blocks[0].fineAB = M / nBlcksA;
-        blocks[0].inizioCR = 0;
-        blocks[0].fineCR = P/nBlcksC;
-
-        if(nBlcksA >= nBlcksC){
-            for(int i = 1; i < nBlcksA; i++){
-                blocks[i].inizioAB = blocks[i-1].fineAB;
-                blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
-                
-                if(blocks[i-1].fineCR != P && blocks[i-1].fineCR != 0){
-                    blocks[i].inizioCR = blocks[i-1].fineCR;
-                    blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
-                }else{
-                    blocks[i].inizioCR = 0;
-                    blocks[i].fineCR = 0;
-                }
-            }
-        }else{
-            for(int i = 1; i < nBlcksC; i++){
-                blocks[i].inizioCR = blocks[i-1].fineCR;
-                blocks[i].fineCR = blocks[i-1].fineCR + P / nBlcksC;
-                
-                if(blocks[i-1].fineAB != M && blocks[i-1].fineAB != 0){
-                    blocks[i].inizioAB = blocks[i-1].fineAB;
-                    blocks[i].fineAB = blocks[i-1].fineAB + M / nBlcksA;
-                }else{
-                    blocks[i].inizioAB = 0;
-                    blocks[i].fineAB = 0;
-                }
-            }
-        }
 
 
         for(int i = 0; i<nThrds; i++){
